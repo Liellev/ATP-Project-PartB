f82d5f24892c52915bf712e1e7e7959b6c0b9db0
AdiLevy489 <adi209363@gmail.com>
2025-04-14 21:00:17 +0300

    DFS running

--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(10, 10);
+        Maze maze = mg.generate(5, 5);
         System.out.println(String.format("Start Position: %s", maze.getStartPosition())); // format "{row,column}"
         System.out.println(String.format("Goal Position: %s", maze.getGoalPosition())); // format "{row,column}"
         SearchableMaze searchableMaze = new SearchableMaze(maze);
