d8eaa8c5eaf02b8a101d96b957c7cb631a2d10bd
AdiLevy489 <adi209363@gmail.com>
2025-05-23 10:06:13 +0300

    Server strategy changes

--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -1,4 +1,40 @@
 package Server;
 
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+import algorithms.search.BestFirstSearch;
+import algorithms.search.SearchableMaze;
+import algorithms.search.Solution;
+
+import java.io.*;
+
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
+    @Override
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+            String maze_file="MazeFromClient";
+            OutputStream outputStream= new FileOutputStream(maze_file);
+
+            Maze maze = (Maze) fromClient.readObject();
+            byte[] maze_byte=maze.toByteArray();
+            SearchableMaze s_maze=new SearchableMaze(maze);
+            BestFirstSearch algorithm= new BestFirstSearch();
+            Solution solution=algorithm.solve(s_maze);
+
+            MyCompressorOutputStream compressorOutputStream = new MyCompressorOutputStream(outputStream);
+            compressorOutputStream.write(maze_byte);
+            InputStream infile= new FileInputStream(maze_file);
+            byte[] compMazeByte=infile.readAllBytes();
+            toClient.writeObject(compMazeByte);
+            toClient.flush();
+
+            fromClient.close();
+            toClient.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
