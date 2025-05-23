0a285807f62b9185c4e2994e56b41809d17ee735
AdiLevy489 <adi209363@gmail.com>
2025-05-23 11:01:38 +0300

    Server+Client implements

--- src/Client/IClientStrategy.java
+++ src/Client/IClientStrategy.java
@@ -6,3 +6,4 @@ import java.io.OutputStream;
 public interface IClientStrategy {
     void clientStrategy(InputStream inFromServer, OutputStream outToServer);
 }
+
--- src/Server/ServerStrategySolveSearchProblem.java
+++ src/Server/ServerStrategySolveSearchProblem.java
@@ -7,34 +7,59 @@ import algorithms.search.SearchableMaze;
 import algorithms.search.Solution;
 
 import java.io.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class ServerStrategySolveSearchProblem implements IServerStrategy {
+
+    String tempDirectoryPath = System.getProperty("java.io.tmpdir");
+
+    public String hashMaze(byte[] maze_byte)  {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashcode=md.digest(maze_byte);
+            BigInteger bigInt=new BigInteger(1,hashcode);
+            return bigInt.toString(16);
+        } catch (NoSuchAlgorithmException e){
+            throw new RuntimeException("hash algo not found",e);
+        }
+    }
+
     @Override
     public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
         try {
             ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
             ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
-            String maze_file="MazeFromClient";
-            OutputStream outputStream= new FileOutputStream(maze_file);
 
             Maze maze = (Maze) fromClient.readObject();
             byte[] maze_byte=maze.toByteArray();
-            SearchableMaze s_maze=new SearchableMaze(maze);
-            BestFirstSearch algorithm= new BestFirstSearch();
-            Solution solution=algorithm.solve(s_maze);
+            String mazeHash=hashMaze(maze_byte);
+            Solution solution;
+            File sol_file=new File(tempDirectoryPath,mazeHash+".sol");
 
-            MyCompressorOutputStream compressorOutputStream = new MyCompressorOutputStream(outputStream);
-            compressorOutputStream.write(maze_byte);
-            InputStream infile= new FileInputStream(maze_file);
-            byte[] compMazeByte=infile.readAllBytes();
-            toClient.writeObject(compMazeByte);
+            if(sol_file.exists()){ //if solution is already done before
+                ObjectInputStream objectInputStream=new ObjectInputStream(new FileInputStream(sol_file));
+                solution=(Solution)objectInputStream.readObject();
+            }
+            else{
+                ObjectOutputStream solution_output=new ObjectOutputStream(new FileOutputStream(sol_file));
+                SearchableMaze s_maze=new SearchableMaze(maze);
+                BestFirstSearch algorithm= new BestFirstSearch();
+                solution=algorithm.solve(s_maze);
+                solution_output.writeObject(solution);
+            }
+
+            toClient.writeObject(solution);
             toClient.flush();
-
             fromClient.close();
             toClient.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
+
 }
